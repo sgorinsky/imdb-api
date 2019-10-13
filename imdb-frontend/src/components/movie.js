1d8f630745b2248255d7c479df4d9bf5c250ef8e
Sam Gorinsky <sgorinsky@gmail.com>
2019-10-13 02:32:31 +0200

    minor fix

--- imdb-frontend/src/components/movie.js
+++ imdb-frontend/src/components/movie.js
@@ -3,9 +3,9 @@ import React, { useState } from 'react'
 const Movie = ({movie, attributes}) => {
     const [toggleShow, setToggleShow ] = useState(false)
     const yr = movie.startyear > '2020' || movie.startyear < '1875' ? '' : `(${movie.startyear})`
-    const run = isNaN(Number(movie.runtimeminutes)) || Number(movie.runtimeminutes) > 550 ? '' : `, ${movie.runtimeminutes} min`
+    const run = isNaN(Number(movie.runtimeminutes)) || Number(movie.runtimeminutes) > 900 ? '' : `, ${movie.runtimeminutes} min`
     const show = toggleShow ? {display: ''} : {display: 'none'}
-    console.log(movie.primarytitle)
+    console.log(movie)
     return (
         <div onClick={() => setToggleShow(!toggleShow)}>
             <p>{movie.primarytitle}{yr}{run}</p>
