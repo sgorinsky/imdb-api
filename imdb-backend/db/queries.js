d2d53df462562d54f179a593877096226db3ea4b
Sam Gorinsky <sgorinsky@gmail.com>
2019-10-13 02:31:53 +0200

    updated queries, joined postgres tables

--- imdb-backend/db/queries.js
+++ imdb-backend/db/queries.js
@@ -1,3 +1,14 @@
+/* 
+original query for joining all 4 tables: 
+create table az_asc as 
+(select distinct primarytitle, startyear, genres, region, language, runtimeminutes, isadult, primaryname, primaryprofession, knownfortitles from 
+    (select directors as nconst, tconst from titlecrew where directors NOT like '%N%'  
+        union 
+            select writers, tconst from titlecrew where writers NOT LIKE '%N%') t1 
+    join titlebasics on t1.tconst = titlebasics.tconst 
+    join titleakas on t1.tconst = titleakas.titleid 
+    join namebasics on t1.nconst = namebasics.nconst);
+*/
 // storing queries since it's messy to include them all in the db's index.js
 const queries = {
     'safetyOn': {
@@ -32,25 +43,25 @@ const queries = {
 } 
 
 // safety on -- not adult
-queries['safetyOn']['az']['asc'] = 'SELECT * FROM az_asc WHERE NOT isadult = \'1\';'
-queries['safetyOn']['az']['desc'] = 'SELECT * FROM az_asc WHERE NOT isadult = \'1\' ORDER BY primarytitle DESC ;'
+queries['safetyOn']['az']['asc'] = 'SELECT * FROM az_asc WHERE NOT isadult = \'1\' ORDER BY primarytitle LIMIT 1500;'
+queries['safetyOn']['az']['desc'] = 'SELECT * FROM az_asc WHERE NOT isadult = \'1\' ORDER BY primarytitle DESC LIMIT 1500;'
 
-queries['safetyOn']['startyear']['asc'] = 'SELECT * FROM year_asc WHERE NOT isadult=\'1\' AND startyear BETWEEN \'1850\' AND \'2020\' ;'
-queries['safetyOn']['startyear']['desc'] = 'SELECT * FROM year_asc WHERE NOT isadult=\'1\' AND startyear BETWEEN \'1850\' AND \'2020\' ORDER BY startyear DESC ;'
+queries['safetyOn']['startyear']['asc'] = 'SELECT * FROM year_asc WHERE NOT isadult=\'1\' AND startyear BETWEEN \'1850\' AND \'2020\' LIMIT 1500;'
+queries['safetyOn']['startyear']['desc'] = 'SELECT * FROM year_asc WHERE NOT isadult=\'1\' AND startyear BETWEEN \'1850\' AND \'2020\' ORDER BY startyear DESC LIMIT 1500;'
 
-queries['safetyOn']['runtime']['asc'] = 'SELECT * FROM runtime_asc WHERE NOT isadult=\'1\' ;'
-queries['safetyOn']['runtime']['desc'] = 'SELECT * FROM runtime_asc WHERE NOT isadult=\'1\' ORDER BY runtimeminutes DESC ;'
+queries['safetyOn']['runtime']['asc'] = 'SELECT * FROM runtime_asc WHERE runtimeminutes BETWEEN \'0\' AND \'A\' AND NOT isadult=\'1\' LIMIT 1500;'
+queries['safetyOn']['runtime']['desc'] = 'SELECT LENGTH(runtimeminutes) as runtime_string_length, * FROM az_asc where runtimeminutes BETWEEN \'0\' AND \'A\' AND length(runtimeminutes) = 3 AND NOT isadult = \'1\' ORDER BY runtime_string_length, runtimeminutes desc LIMIT 1500;' 
 
 // safety filter off
-queries['safetyOff']['az']['asc'] = 'SELECT * FROM az_asc ;'
-queries['safetyOff']['az']['desc'] = 'SELECT * FROM az_asc ORDER BY primarytitle DESC ;'
+queries['safetyOff']['az']['asc'] = 'SELECT * FROM az_asc ORDER BY primarytitle ASC LIMIT 1500;'
+queries['safetyOff']['az']['desc'] = 'SELECT * FROM az_asc ORDER BY primarytitle DESC LIMIT 1500;'
 
-queries['safetyOff']['startyear']['asc'] = 'SELECT * FROM year_asc WHERE startyear BETWEEN \'1850\' AND \'2020\' ;'
-queries['safetyOff']['startyear']['desc'] = 'SELECT * FROM year_asc WHERE startyear BETWEEN \'1850\' AND \'2020\' ORDER BY startyear DESC ;'
+queries['safetyOff']['startyear']['asc'] = 'SELECT * FROM year_asc WHERE startyear BETWEEN \'1850\' AND \'2020\' LIMIT 1500;'
+queries['safetyOff']['startyear']['desc'] = 'SELECT * FROM year_asc WHERE startyear BETWEEN \'1850\' AND \'2020\' ORDER BY startyear DESC LIMIT 1500;'
 
-queries['safetyOff']['runtime']['asc'] = 'SELECT * FROM runtime_asc ;'
-queries['safetyOff']['runtime']['desc'] = 'SELECT * FROM runtime_asc ORDER BY runtimeminutes DESC ;'
+queries['safetyOff']['runtime']['asc'] = 'SELECT * FROM runtime_asc WHERE runtimeminutes BETWEEN \'0\' AND \'A\' LIMIT 1500;'
+queries['safetyOff']['runtime']['desc'] = 'SELECT LENGTH(runtimeminutes) as runtime_string_length, * FROM az_asc where runtimeminutes < \'A\' and length(runtimeminutes) = 3 ORDER BY runtime_string_length, runtimeminutes desc LIMIT 1500;'
 
 // for searching
-queries['all'] = 'SELECT * FROM az_asc'
+queries['all'] = 'SELECT * FROM az_asc ORDER BY primarytitle ASC'
 module.exports = queries 
